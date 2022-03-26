d5fe0a87cc36f53b0a425b7cf80761ba43eb9701
LAPTOP-BQ9GIK6U\su <tmdduq5498@gmail.com>
2022-03-26 22:20:33 +0900

    셀프 넘버 수정

--- src/Function/BaekJoon4673.java
+++ src/Function/BaekJoon4673.java
@@ -16,17 +16,17 @@ public class BaekJoon4673 {
 		// 없는 숫자를 셀프 넘버라고 한다. 100보다 작은 셀프 넘버는 총 13개가 있다. 1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97
 		// 10000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 출력하는 프로그램을 작성하시오
 		
-		boolean[] selfNum = new boolean[10001]; // 1~10000까지의 숫자
+		boolean[] isNotSelfNum = new boolean[10001]; // 1~10000까지의 숫자
 		
 		for(int i=0; i<10001; i++) {
 			int n = d(i);
 			if(n<=10000) {
-				selfNum[n] = true;
+				isNotSelfNum[n] = true;
 			}
 		}
 		
 		for(int i=0; i<10001; i++) {
-			if(!selfNum[i]) {
+			if(!isNotSelfNum[i]) {
 				System.out.println(i);
 			}
 		}
