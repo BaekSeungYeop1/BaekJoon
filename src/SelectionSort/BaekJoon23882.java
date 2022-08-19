8188db1779b3a1c4d5daaf98fcb628a2ce294eaa
DESKTOP-TPQHNMR\baek <tmdduq5498@gmail.com>
2022-08-20 00:23:49 +0900

    선택 정렬 예제 추가

---
+++ src/SelectionSort/BaekJoon23882.java
@@ -0,0 +1,63 @@
+package SelectionSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon23882 {
+	
+	private static int K,N;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		solve(arr);
+		
+	}
+
+	private static void solve(int[] arr) {
+		int temp;
+		int maxIndex;
+		int cnt = 0;
+		
+		for(int i = arr.length-1; i>0; i--) {
+			maxIndex = i;
+			for(int j = i-1; j>=0; j--) {
+				if(arr[maxIndex]<arr[j]) {
+					maxIndex = j;
+				}
+			}
+			if(i != maxIndex) {
+				temp = arr[maxIndex];
+				arr[maxIndex] = arr[i];
+				arr[i] = temp;
+				cnt++;
+				
+				if(cnt == K) {
+					StringBuilder sb = new StringBuilder();
+					for(int j = 0; j<arr.length; j++) {
+						sb.append(arr[j]).append(" ");
+					}
+					System.out.println(sb);
+					return;
+				}
+			}
+		}
+		System.out.println(-1);
+		
+	}
+
+}
