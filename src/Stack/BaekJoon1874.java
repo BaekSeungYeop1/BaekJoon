7cfb2fd9b0088d6e490d6d1f0f9e700b7ac0f557
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2022-09-18 01:54:34 +0900

    스택 - 스택 수열

---
+++ src/Stack/BaekJoon1874.java
@@ -0,0 +1,39 @@
+package Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BaekJoon1874 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int start = 0;
+		
+		while(N-- > 0) {
+			int num = Integer.parseInt(br.readLine());
+			if(start < num) {
+				for(int i = start+1; i<= num; i++ ) {
+					stack.push(i);
+					sb.append("+").append("\n");
+				}
+				start = num;
+			}
+			else if(stack.peek() != num) {
+				System.out.print("No");
+				return;
+			}
+			
+			stack.pop();
+			sb.append("-").append("\n");
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
