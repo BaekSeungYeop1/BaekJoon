092cf6ff62012034ca57fe435f7f24af32f4cde8
DESKTOP-NT05U6L\Yeop <tmdduq5498@gmail.com>
2022-09-27 02:06:29 +0900

    자료구조 - 요세푸스 문제0

---
+++ src/DataStructure/BaekJoon11866.java
@@ -0,0 +1,39 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BaekJoon11866 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		Queue<Integer> queue = new LinkedList<>();
+		
+		for(int i = 1; i <= N; i++) {
+			queue.offer(i);
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		while(queue.size() != 1) {
+			
+			for(int i = 0; i<K-1; i++) {
+				queue.offer(queue.poll());
+			}
+			sb.append(queue.poll()).append(", ");
+		}
+		if(queue.size() == 1) {
+			sb.append(queue.poll()).append(">");
+		}
+		System.out.print(sb);
+	}
+
+}
