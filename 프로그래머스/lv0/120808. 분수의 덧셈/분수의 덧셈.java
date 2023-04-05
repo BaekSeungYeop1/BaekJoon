e54e4cf684feb183bd245af8eba17e0f381842de
BaekSeungYeop1 <66419276+BaekSeungYeop1@users.noreply.github.com>
2023-04-05 21:11:53 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -2,10 +2,8 @@ class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
 
-        answer[1] = lcm(denom1,denom2);
-        int num1 = answer[1]/denom1;
-        int num2 = answer[1]/denom2;
-        answer[0] = numer1*num1 + numer2*num2;
+        answer[0] = numer1*denom2 + numer2*denom1;
+        answer[1] = denom1*denom2;
 
         int gcd = gcd(answer[0],answer[1]);
         answer[0] /= gcd;
