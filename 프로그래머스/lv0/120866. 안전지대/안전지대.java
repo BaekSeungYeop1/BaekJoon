f0ade9bd630322677b56ef1397c832d3aa9f22f0
BaekSeungYeop1 <66419276+BaekSeungYeop1@users.noreply.github.com>
2023-04-06 17:20:58 +0900

    [level 0] Title: 안전지대, Time: 0.07 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    private static int[][] safeBoard;
+    
+    public int solution(int[][] board) {
+        safeBoard = new int[board.length][board.length];
+        for (int i = 0; i<board.length; i++){
+            for (int j = 0; j< board.length; j++){
+                if (board[i][j] == 1){
+                    safeZone(i, j);
+                }
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i<safeBoard.length; i++){
+            for (int j = 0; j<safeBoard.length; j++){
+                if (safeBoard[i][j] == 0){
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+    private static void safeZone(int a,int b) {
+        for (int i = a-1; i <= a+1; i++){
+            for (int j= b-1; j <= b+1; j++){
+                if (i < 0 || i > safeBoard.length - 1 || j < 0 || j > safeBoard.length-1) {
+                    continue;
+                }
+                safeBoard[i][j] = 1;
+            }
+        }
+    }
+}
