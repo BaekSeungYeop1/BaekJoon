a8c6a3c0ee3a4d0c1fa918676360390880e5d37b
BaekSeungYeop1 <66419276+BaekSeungYeop1@users.noreply.github.com>
2023-04-11 21:00:53 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -4,11 +4,11 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        while(n > 0){
-            answer += n % 10;
-            n = n/10;
+        while(n>0){
+            answer += n%10;
+            n /= 10;
         }
-
+        
         return answer;
     }
 }
