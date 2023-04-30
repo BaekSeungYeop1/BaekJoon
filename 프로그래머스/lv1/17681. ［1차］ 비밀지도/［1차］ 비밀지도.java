580964f19af1cf24e61aa15a007f9d30fe504c14
BaekSeungYeop1 <66419276+BaekSeungYeop1@users.noreply.github.com>
2023-04-30 11:28:13 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.30 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+class Solution {
+     public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String[] result = new String[n];
+        for (int i = 0; i<n; i++){
+            String str;
+            str = Integer.toBinaryString(arr1[i] | arr2[i]);
+            str = String.format("%" + n + "s",str);
+            answer[i] = str;
+        }
+
+        for (int i = 0; i< answer.length; i++){
+            String s = "";
+            for (int j = 0; j<answer.length; j++){
+                String c = String.valueOf(answer[i].charAt(j));
+                if (c.equals("1")){
+                    s += "#";
+                }
+                else{
+                    s += " ";
+                }
+            }
+            result[i] = s;
+        }
+
+        return result;
+}
+}
