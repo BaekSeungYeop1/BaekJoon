e5fff7c94e825cd3727a6ec299018a31e8ff8c30
BaekSeungYeop1 <66419276+BaekSeungYeop1@users.noreply.github.com>
2024-04-04 11:26:39 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+
+/*
+
+  */
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringTokenizer st =new StringTokenizer(br.readLine(), " ");
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder(s);
+        if (s.contentEquals(sb.reverse())){
+            System.out.println(1);
+        }
+        else
+            System.out.println(0);
+
+    }
+}
