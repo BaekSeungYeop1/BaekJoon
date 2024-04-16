1a974e3080906de9f682e91bd354bba05b7fafac
BaekSeungYeop1 <66419276+BaekSeungYeop1@users.noreply.github.com>
2024-04-16 21:26:12 +0900

    [Bronze II] Title: 공 바꾸기, Time: 148 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i<arr.length; i++){
+            arr[i] = i+1;
+        }
+        int temp;
+        for (int i = 0; i<M; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int idx1 = Integer.parseInt(st.nextToken());
+            int idx2 = Integer.parseInt(st.nextToken());
+            temp = arr[idx1-1];
+            arr[idx1-1] = arr[idx2-1];
+            arr[idx2-1] = temp;
+        }
+        for (int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
