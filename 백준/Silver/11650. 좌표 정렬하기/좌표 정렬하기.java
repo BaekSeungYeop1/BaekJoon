0c20fdf6f1d62a50c12202b0827deb40ba3e9548
BaekSeungYeop1 <66419276+BaekSeungYeop1@users.noreply.github.com>
2024-04-18 22:33:48 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 848 ms, Memory: 54168 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i< N ; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
+
+
+
+    }
+    /* comparator
+        public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        Integer arr[][] = new Integer[N][2];
+
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<Integer[]>() {
+            @Override
+            public int compare(Integer[] o1, Integer[] o2) {
+                if(o1[0].equals(o2[0])) {
+                    return o1[1] - o2[1];
+                }
+                else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+        for(int i=0; i<N; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+        }
+
+        System.out.println(sb);
+    }*/
+}
